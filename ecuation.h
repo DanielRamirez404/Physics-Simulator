5036a1e5d1a6a11ac8afa105f1cdf9cd85fe80f1
DanielRamirez404 <danielramirezabou404@gmail.com>
2023-08-30 17:21:57 -0400

    Added more 'blueprints' to ecuation.h

--- ecuation.h
+++ ecuation.h
@@ -3,18 +3,39 @@
 #include <string>
 #include <string_view>
 
-template <typename T> struct Variable {
-  std::string_view name{};
-  T value{};
-};
+namespace Math {
+  template <typename T> struct Variable {
+    char identifier{};
+    T value{};
+  };
 
-template <typename T> class Ecuation {
-private:
-  std::string formula{};
-  Variable* variables{};
-public:
-  Ecuation(const char* myFormula, std::string_view* myVariableNames) : formula(myFormula), variableNames(myVariableNames) {};
-  Ecuation(const Ecuation&) = delete;
-  Ecuation& operator=(const Ecuation&) = delete;
-  T solveFor();
-};
+  template <typename T> class Operation {
+  private:
+    std::string formula{};
+  public:
+    Operation(const char* myFormula) : formula(myFormula) {};
+    T solve();
+  };
+
+  template <typename T> T Ecuation<T>::solveFor(char identifier) {
+    //todo: make algorithm to solve operations
+  }
+
+  template <typename T> class Ecuation {
+  private:
+    std::string formula{};
+    Variable* variables{};
+  public:
+    Ecuation(const char* myFormula, std::string_view* myVariableNames) : formula(myFormula), variableNames(myVariableNames) {};
+    Ecuation(const Ecuation&) = delete;
+    Ecuation& operator=(const Ecuation&) = delete;
+    T solveFor(char identifier);
+  };
+
+  template <typename T> T Ecuation<T>::solveFor(char identifier) {
+    /*  if (doesIndentifierExist(identifier))
+     *  1. operation = Ecuation.getOperationToCalculate(identifier)
+     *  2. return operation.solve();
+     */
+  }
+}
